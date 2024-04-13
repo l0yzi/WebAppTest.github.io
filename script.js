dfd5aacb7c93caf5aa0ede3a44014cdf28e8657e
l0yzi <sinnloseexistenz@gmail.co>
2024-04-14 02:15:22 +0300

    1

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function getStorage () {
     storageScreen.classList.remove('hide')
     storageScreen.classList.add('visible_storage_screen')
 }
+
+var BackButton = WebApp.BackButton;
+BackButton.show();
+BackButton.onClick(function() {
+  WebApp.showAlert("Нет пути назад!");
+  BackButton.hide();
+});
+WebApp.onEvent('backButtonClicked', function() {
+  /* код */
+});
