0f6ceae0a1773598c817b559b3934817dd6125dd
l0yzi <sinnloseexistenz@gmail.co>
2024-04-14 03:21:09 +0300

    test

---
+++ preloader.html
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="reset.css">
+</head>
+<body>
+    <div class="visible_preloader" id="preloader">
+        <img src="assets/logo.png" class="pr_img_hami">
+        <img src="assets/img_tocen.png" class="pr_img_logo">
+        <p>Loading the HAMI<br>project...</p>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // screens
 let mainScreen = document.getElementById('main_screen')
 let storageScreen = document.getElementById('storage_screen')
-
+let preloaderScreen = document.getElementById('preloader')
 // Кнопки
 let btnStorage = document.getElementById('storage_btn')
 
@@ -14,3 +14,13 @@ function getStorage () {
     storageScreen.classList.add('visible_storage_screen')
 }
 
+
+window.onload = function () {
+    if(preloaderScreen.classList.contains('visible_preloader')){
+        window.setTimeout(function () {
+            preloaderScreen.classList.remove('visible_preloader')
+            preloaderScreen.classList.add('hide')
+            window.location="main.html"
+        }, 8000);
+    } 
+}
